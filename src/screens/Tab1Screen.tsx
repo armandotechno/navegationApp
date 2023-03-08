aebf46b9d9f9fa668b791a8735d8c89b6304b1bb
Armando Capriles <armandotechno12@gmail.com>
2023-03-08 09:43:42 -0400

    Error corregido en Tab1Screen

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,5 +1,5 @@
-import Icon from 'react-native-vector-icons/Ionicons'
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 import { styles } from '../theme/appTheme';
 
 export const Tab1Screen = () => {
@@ -7,16 +7,17 @@ export const Tab1Screen = () => {
       <View style={ styles.globalMargin }>
         <Text style={ styles.title }> Iconos </Text>
 
-        <Text>
-          {/* <Icon name="airplane-outline" size={ 80 } color="#900" />
-          <Icon name="attach-outline" size={ 80 } color="#1122c0" />
-          <Icon name="alarm-outline" size={ 80 } color="#1d011b" />
-          <Icon name="construct-outline" size={ 80 } color="#0a94480" />
-          <Icon name="eye-outline" size={ 80 } color="#eb10ff" />
-          <Icon name="eye-off-outline" size={ 80 } color="#00992e" />
-          <Icon name="finger-print-outline" size={ 80 } color="#d16105" />
-          <Icon name="ear-outline" size={ 80 } color="#00ffff" /> */}
-        </Text>
+        <View>
+          <Text>
+            <Icon name="airplane-outline" size={ 80 } color="#900" /> 
+            <Icon name="attach-outline" size={ 80 } color="#1122c0" /> 
+            <Icon name="alarm-outline" size={ 80 } color="#1d011b" /> 
+            <Icon name="eye-outline" size={ 80 } color="#eb10ff" /> 
+            <Icon name="eye-off-outline" size={ 80 } color="#00992e" />
+            <Icon name="finger-print-outline" size={ 80 } color="#d16105" />
+            <Icon name="ear-outline" size={ 80 } color="#00ffff" /> 
+          </Text>
+        </View>
       </View>
     )
 }
